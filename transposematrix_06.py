f6235a85dac7610043d9767970eb2695fe48296c
jamieyu0914 <jamieyu0914@gmail.com>
2023-04-10 12:35:00 +0800

    CH02_06 transposematrix_06.py

---
+++ transposematrix_06.py
@@ -0,0 +1,26 @@
+#CH02_06
+#transpose matrix
+
+#實作一個4*4二維陣列的轉至矩陣
+
+arrA = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+N=4
+#宣告4X4陣列arr
+arrB = [[None] * N for row in range(N)]
+
+print('[原設定的矩陣內容]')
+for i in range(4):
+    for j in range(4):
+        print('%d' %arrA[i][j],end='\t')
+    print()
+
+#進行矩陣轉置動作
+for i in range(4):
+    for j in range(4):
+        arrB[i][j] = arrA[j][i]
+
+print('[轉置矩陣的內容為]')
+for i in range(4):
+    for j in range(4):
+        print('%d' %arrB[i][j],end='\t')
+    print()
